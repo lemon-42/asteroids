1127991f75f9fcac4ddca3cb9a59e8fba51c277c
lemon_42 <etclegg96@gmail.com>
2024-10-28 16:47:56 +0100

    Add clock to control FPS in game

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from constants import *
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,11 +15,10 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
-    
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
